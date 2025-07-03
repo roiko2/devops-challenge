d3b3fbd908826e63e9816358d60b388be8e1c6b1
Roiko <roikor88@gmail.com>
2025-07-03 20:32:53 +0300

    Add working fetch secrets

--- devops-challenge/config.py
+++ devops-challenge/config.py
@@ -4,3 +4,5 @@ IMAGE_REPOSITORY = os.getenv("IMAGE_REPOSITORY", "https://hub.docker.com/reposit
 GITHUB_REPOSITORY = os.getenv("GITHUB_REPOSITORY", "https://github.com/roiko2/devops-challenge")
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", "")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", "")
+AWS_REGION = os.getenv("AWS_REGION", "eu-west-1")
+TABLE_NAME = os.getenv("TABLE_NAME", "devops-challenge")
--- devops-challenge/routers/app_router.py
+++ devops-challenge/routers/app_router.py
@@ -1,10 +1,20 @@
 from flask import Blueprint, jsonify
-from config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+import boto3
+from config import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_REGION, TABLE_NAME
 
 app_bp = Blueprint('app', __name__)
 
 @app_bp.route('/secret', methods=['GET'])
-def health_check():
+def get_secret_code(secret_code='theDoctor'):
+    session = boto3.Session(
+        aws_access_key_id = AWS_ACCESS_KEY_ID,
+        aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
+        region_name = AWS_REGION
+    )
+    dynamodb = session.resource('dynamodb')
+    table = dynamodb.Table(TABLE_NAME)
+    response = table.get_item(Key={'codeName': f'{secret_code}'})
+
     return jsonify({
-        'secret_code': f'test'
+        'secret_code': f'{response.get("Item")["secretCode"]}'
     })
